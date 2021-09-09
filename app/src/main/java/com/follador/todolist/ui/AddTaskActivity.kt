50a2012ea9cd26c0bbe0338e92a2db58fd4c76f5
IgorFollador <igorledf@hotmail.com>
2021-09-09 01:30:08 -0300

    Implementing date selection and text entry when creating tasks

--- app/src/main/java/com/follador/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/follador/todolist/ui/AddTaskActivity.kt
@@ -1,4 +1,35 @@
 package com.follador.todolist.ui
 
-class AddTaskActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.follador.todolist.databinding.ActivityAddTaskBinding
+import com.follador.todolist.extensions.format
+import com.follador.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+    }
+
 }
